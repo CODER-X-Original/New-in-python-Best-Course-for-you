57de7ad5c47f1ade8c3fb0291f19cf04b4d3f58d
DoNotDistrub <110594137+DoNotDistrub@users.noreply.github.com>
2022-08-20 03:12:58 -0700

    Add files via upload

---
+++ Output In python.py
@@ -0,0 +1,11 @@
+# Do you know eveything you see on your screen is printed by computer
+# the print function is very usefull to print any thing ofn screen
+# Now we know print function is used to print anything on screen
+
+# Now lets talk about the syntax of print function in python
+
+# This is how we write a print function
+
+print("That's how we write print function in python")
+
+# First we write print then bracket and then quotation mark after that we wrte the name we want to print in between the quotion mark
