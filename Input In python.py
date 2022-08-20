5475cfb45816285bda58a4e4e49c2f10307122b6
DoNotDistrub <110594137+DoNotDistrub@users.noreply.github.com>
2022-08-20 04:04:19 -0700

    Initial commit

---
+++ Input In python.py
@@ -0,0 +1,10 @@
+# As we learn you write a keyword print function to see the code on screen .
+# Sonetimes we also need to ask something from user .
+# For that we use special keyword called input 
+# Now we know that input is used for asking somthing from user 
+
+# Now Let's talk about input Syntax
+
+input("How are you")
+
+# First we Write input then bracket,quotion mark 
