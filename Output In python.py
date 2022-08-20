f77b34bad9b22436968ebee85cd84422ae4cda31
DoNotDistrub <110594137+DoNotDistrub@users.noreply.github.com>
2022-08-20 03:19:10 -0700

    Update Output In python.py

--- Output In python.py
+++ Output In python.py
@@ -1,11 +1,12 @@
-# Do you know eveything you see on your screen is printed by computer
-# the print function is very usefull to print any thing ofn screen
-# Now we know print function is used to print anything on screen
+# Do you know eveything you see on your screen is printed by computer .
+# the print function is very usefull to print any thing ofn screen .
+# Now we know print function is used to print anything on screen .
 
-# Now lets talk about the syntax of print function in python
+# Now lets talk about the syntax of print function in python .
 
-# This is how we write a print function
+# This is how we write a print function .
 
 print("That's how we write print function in python")
 
-# First we write print then bracket and then quotation mark after that we wrte the name we want to print in between the quotion mark
+# First we write print then bracket and then quotation mark after that we wrte the name we want to print in between the quotion mark .
+ 
