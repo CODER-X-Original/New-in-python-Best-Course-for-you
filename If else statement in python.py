74f30110fbbad6736719cdb30d357275ce8df0fc
DoNotDistrub <110594137+DoNotDistrub@users.noreply.github.com>
2022-08-23 10:34:02 -0700

    Initial commit

---
+++ If else statement in python.py
@@ -0,0 +1,16 @@
+# if else stament is used for a contidion we not know confirmly for example I say that if their is raining I am going to put my boots and else I am going to put my normal suit 
+# If else statement are similar to the example I have given in pyhton
+
+# Now we know that what are if else statement in python
+# So lets talk about it's syntax
+
+# First I am going to create a variable
+
+Input_from_User = input(str("Is This time raining or not (yes or no) : "))
+
+
+if Input_from_User == "yes":
+    print("I am going to put on my necessary suit for the rain")
+
+else :
+    print("I am going in my normal suit")
